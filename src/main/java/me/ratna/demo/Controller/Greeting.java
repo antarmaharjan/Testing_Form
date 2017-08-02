6c14296c2297b739b061e5a3f2596fedc175f7e4
Ratna <me.ratna.maharjan@gmail.com>
2017-08-02 14:27:20 -0400

    This is the first commit

---
+++ src/main/java/me/ratna/demo/Controller/Greeting.java
@@ -0,0 +1,25 @@
+package me.ratna.demo.Controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class Greeting {
+    private long id;
+    private String content;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
