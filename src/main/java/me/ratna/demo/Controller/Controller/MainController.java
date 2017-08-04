beefa33819699e0cb8309eea7aa714a86600ca92
Ratna <me.ratna.maharjan@gmail.com>
2017-08-04 16:58:25 -0400

    This is the first commit

--- src/main/java/me/ratna/demo/Controller/Controller/MainController.java
+++ src/main/java/me/ratna/demo/Controller/Controller/MainController.java
@@ -1,22 +1,29 @@
-import me.ratna.demo.Controller.Greeting;
+package me.ratna.demo.Controller.Controller;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import javax.validation.Valid;
+
 @Controller
-public class GreetingController {
+public class MainController {
+
     @GetMapping("/greeting")
-
     public String greetingForm(Model model){
-
         model.addAttribute("greeting", new Greeting());
         return "greeting";
-
     }
+
     @PostMapping("/greeting")
-    public String greetingSubmit(@ModelAttribute Greeting greeting){
+    public String greetingSubmit(@Valid Greeting greeting, BindingResult bindingResult){
+        if (bindingResult.hasErrors()){
+            return "greeting";
+        }
+
         return "result";
     }
 }
